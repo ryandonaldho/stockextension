ef8813433a95dddda1eafa4d7fd92d9ada6f986b
Ryan Ho <ryandonaldho@gmail.com>
2019-12-14 21:16:13 -0600

    working displaying all infomation

--- src/ui.js
+++ src/ui.js
@@ -2,6 +2,15 @@ class UI {
     constructor(){
         this.cardElement = document.querySelector('#main-card');
         this.cardTitle = document.querySelector('.card-title');
+        this.cardOpenPrice = document.querySelector('#open-value');
+        this.cardHighPrice = document.querySelector('#high-value');
+        this.cardLowPrice = document.querySelector('#low-value');
+        this.cardPrice = document.querySelector('#price-value');
+        this.cardVolume = document.querySelector('#volume-value');
+        this.cardTradingDay = document.querySelector('#tradeday-value');
+        this.cardPreviousClose = document.querySelector('#prevclose-value');
+        this.cardChange = document.querySelector('#change-value');
+        this.cardPercent = document.querySelector('#percent-value');
     }
 
     initialState(){
@@ -9,10 +18,33 @@ class UI {
     }
 
     displayCard(title,data){
-        console.log(data);
+        // Get Values
+        let openValue = data["Global Quote"]["02. open"];
+        let highValue = data["Global Quote"]["03. high"];
+        let lowValue = data["Global Quote"]["04. low"];
+        let priceValue = data["Global Quote"]["05. price"];
+        let volumeValue = data["Global Quote"]["06. volume"];
+        let latestTradingDayValue = data["Global Quote"]["07. latest trading day"];
+        let previousCloseValue = data["Global Quote"]["08. previous close"];
+        let changeValue = data["Global Quote"]["09. change"];
+        let changePercentValue = data["Global Quote"]["10. change percent"];
+
+        //console.log(data);
+
+        // Set Card Element Values
         this.cardElement.style.display = "block";
-        console.log(title);
         this.cardTitle.innerHTML = title;
+
+        this.cardOpenPrice.innerHTML = openValue;
+        this.cardHighPrice.innerHTML = highValue;
+        this.cardLowPrice.innerHTML = lowValue;
+        this.cardPrice.innerHTML = priceValue;
+        this.cardVolume.innerHTML = volumeValue;
+        this.cardTradingDay.innerHTML = latestTradingDayValue;
+        this.cardPreviousClose.innerHTML = previousCloseValue;
+        this.cardChange.innerHTML = changeValue;
+        this.cardPercent.innerHTML = changePercentValue;
+
     }
 }
 
