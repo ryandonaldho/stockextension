6dadad5948f4826251e080e01b6b3f4366fa0472
Ryan Ho <ryandonaldho@gmail.com>
2020-04-09 20:43:50 -0500

    Use Local Storage instead of chrome storage

--- src/popup.js
+++ src/popup.js
@@ -1,12 +1,17 @@
 import { ui } from "./ui.js";
-import { getSymbol, getStockQuote, getMutipleStocks, formatData } from "./helpers.js";
+import {
+  getSymbol,
+  getStockQuote,
+  getMutipleStocks,
+  formatData,
+} from "./helpers.js";
 
 let data = null;
 let elems = null;
 let instances = null;
 let state = {
-  selectedStock: '',
-  selectedStockToDelete: ''
+  selectedStock: "",
+  selectedStockToDelete: "",
 };
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -15,19 +20,19 @@ document.addEventListener("DOMContentLoaded", function () {
     data,
     minLength: 1,
   });
-  let confirmDeleteModal = document.querySelectorAll('.modal');
+  let confirmDeleteModal = document.querySelectorAll(".modal");
   let modalInstance = M.Modal.init(confirmDeleteModal, {
-    dismissible: true
+    dismissible: true,
   });
 
   let searchResultResponse = getSymbol();
   searchResultResponse
-    .then(data => {
+    .then((data) => {
       let formattedData = formatData(data);
       //console.log(formattedData);
       updateSearchResult(formattedData);
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 
   initialize();
 
@@ -35,140 +40,30 @@ document.addEventListener("DOMContentLoaded", function () {
     .querySelector(".autocomplete-content.dropdown-content")
     .addEventListener("click", handleStockSelection);
 
-
   // Add Button Listener
-  document.querySelector('#add-button').addEventListener("click", handleAddStock);
+  document
+    .querySelector("#add-button")
+    .addEventListener("click", handleAddStock);
 
   // Delete Stock Button Listener
-  document.querySelector('#watch-list tbody').addEventListener("click", getStockToBeDeleted);
+  document
+    .querySelector("#watch-list tbody")
+    .addEventListener("click", getStockToBeDeleted);
 
   // Confirm Delete Stock Button Listener
-  document.querySelector('#confirm-delete-button').addEventListener("click", handleDelete);
+  document
+    .querySelector("#confirm-delete-button")
+    .addEventListener("click", handleDelete);
 
   // Opening link to yahoo finance for symbol
-  document.querySelector('#card-link').addEventListener("click", handleOpenNewTab);
+  document
+    .querySelector("#card-link")
+    .addEventListener("click", handleOpenNewTab);
 });
 
 function initialize() {
   ui.hideStockInfoCard();
-  getLatestUpdate().then(() => {
-    getDataAndDisplay();
-  })
-}
-
-function getLatestUpdate() {
-  return new Promise((resolve) => {
-    getAllStocksInWatchlist().then((symbols) => {
-      console.log(symbols);
-      let stocksString = formatSymbols(symbols);
-      console.log(stocksString);
-      getMutipleStocks(stocksString).then((data) => {
-        let watchlist = [];
-        for (const stock in data) {
-          watchlist.push(data[stock]["quote"]);
-        }
-        //console.log(watchlist);
-        console.log("setting local storage with new data");
-        chrome.storage.local.set({ stocks: watchlist });
-        resolve();
-      });
-    })
-  })
-}
-
-function getAllStocksInWatchlist() {
-  return new Promise(resolve => {
-    chrome.storage.local.get(['stocks'], function (result) {
-      let symbols = [];
-      if (result.stocks != undefined) {
-        console.log(result.stocks);
-        result.stocks.forEach((stock) => {
-          symbols.push(stock.symbol);
-        })
-      }
-      console.log(symbols);
-      resolve(symbols);
-    });
-  })
-
-}
-
-// return a string with the symbols seperated by commas
-function formatSymbols(symbols) {
-  return symbols.join();
-}
-
-function getStockToBeDeleted(e) {
-  if (e.target.parentElement.classList.contains('btn-delete')) {
-    const stockSymbol = e.target.parentElement.parentElement.parentElement.getAttribute("data-id");
-    console.log(stockSymbol);
-    state.selectedStockToDelete = stockSymbol;
-  }
-}
-
-function handleDelete(e) {
-  const stockSymbol = state.selectedStockToDelete;
-  console.log(stockSymbol);
-  if (stockSymbol !== '') {
-    // get sync storage and update 
-    deleteFromWatchList(stockSymbol).then(() => getDataAndDisplay());
-    state.selectedStockToDelete = '';
-    ui.displayToastMessage(`${stockSymbol} have been successfully deleted from the watchlist`, 'success');
-  }
-  else {
-    console.log("Error selectedStockToDelete is empty")
-  }
-
-}
-
-function deleteFromWatchList(stockSymbol) {
-  //console.log(stockSymbol);
-  return new Promise(resolve => {
-    chrome.storage.local.get(['stocks'], function (result) {
-      let watchlist = result.stocks;
-      console.log(watchlist);
-      watchlist = watchlist.filter(function (stock) {
-        return stock.symbol !== stockSymbol;
-      });
-      console.log(watchlist);
-      chrome.storage.local.set({ stocks: watchlist });
-      resolve();
-    });
-  })
-}
-
-function handleStockSelection(e) {
-  let target = e.target;
-  // Get Stock Symbol
-  if (target) {
-    let symbol = e.target.parentElement.getAttribute("data-stock-symbol");
-    document.querySelector('#autocomplete-input').value = '';
-    let quoteResultResponse = getStockQuote(symbol);
-    quoteResultResponse.then(data => {
-      console.log(data);
-      ui.displayCard(data);
-      updateSelectedStock(data);
-    }).catch(err => console.log(err));
-  }
-}
-
-function handleAddStock(e) {
-  //console.log('clicked', state["selectedStock"]);
-  let stock = state["selectedStock"]
-  // Check if stock already exist
-  const found = checkStockAlreadyExist(stock);
-  found.then((result) => {
-    if (result) {
-      ui.displayToastMessage(`${stock.symbol} already exists in the watchlist`, 'error');
-    }
-    else {
-      // Add stock to portfolio state
-      addStockToPortfolio(stock).then(() => getDataAndDisplay());
-      ui.displayToastMessage(`${stock.symbol} have been successfully added to the watchlist`, 'success');
-    }
-  })
-  // get sync storage and update ui
-  //getDataAndDisplay();
+  getDataAndDisplay();
 }
 
 function handleOpenNewTab(e) {
@@ -181,53 +76,95 @@ function updateSearchResult(data) {
   instance.updateData(data);
 }
 
-// update state
-function updateSelectedStock(data) {
-  console.log(data);
-  state["selectedStock"] = data;
+async function handleStockSelection(e) {
+  let target = e.target;
+  // Get Stock Symbol
+  if (target) {
+    let symbol = e.target.parentElement.getAttribute("data-stock-symbol");
+    document.querySelector("#autocomplete-input").value = "";
+    try {
+      let quoteResultResponse = await getStockQuote(symbol);
+      console.log(quoteResultResponse);
+      ui.displayCard(quoteResultResponse);
+      state["selectedStock"] = quoteResultResponse;
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
 
-// add stock to watchlist state
-function addStockToPortfolio(stock) {
+function handleAddStock(e) {
+  let stocks = [];
+  stocks = JSON.parse(localStorage.getItem("stocks"));
+  if (stocks == undefined) {
+    stocks = [];
+  }
+  const stockToAdded = state["selectedStock"]["symbol"];
+  const found = stocks.find((stock) => stock == stockToAdded);
+  if (found) {
+    ui.displayToastMessage(
+      `${state["selectedStock"]["symbol"]} already exists in the watchlist`,
+      "error"
+    );
+  } else {
+    stocks.push(stockToAdded);
+    localStorage.setItem("stocks", JSON.stringify(stocks));
+  }
+  getDataAndDisplay();
+}
+
+// return a string with the symbols seperated by commas
+function formatSymbols(symbols) {
+  return symbols.join();
+}
+
+async function getDataAndDisplay() {
+  let stocks = [];
   let watchlist = [];
-  return new Promise(resolve => {
-    chrome.storage.local.get(['stocks'], function (result) {
-      //console.log(result.stocks);
-      if (result.stocks != null) {
-        watchlist = result.stocks;
-        //console.log(watchlist);
-      }
-      watchlist.push(stock);
-      //console.log("adding..", watchlist);
-      chrome.storage.local.set({ stocks: watchlist });
-      resolve();
-    });
-  })
-
+  stocks = JSON.parse(localStorage.getItem("stocks"));
+  if (stocks == undefined) {
+    stocks = [];
+  }
+  if (stocks.length > 0) {
+    let stocksString = formatSymbols(stocks);
+    let response = await getMutipleStocks(stocksString);
+    // convert to array
+    for (const stock in response) {
+      watchlist.push(response[stock]["quote"]);
+    }
+  }
+  ui.displayPortfolio(watchlist);
 }
 
-function getDataAndDisplay() {
-  chrome.storage.local.get(['stocks'], function (result) {
-    console.log('display', result);
-    if (result.stocks != undefined)
-      ui.displayPortfolio(result.stocks);
-  });
+function getStockToBeDeleted(e) {
+  if (e.target.parentElement.classList.contains("btn-delete")) {
+    const stockSymbol = e.target.parentElement.parentElement.parentElement.getAttribute(
+      "data-id"
+    );
+    console.log(stockSymbol);
+    state.selectedStockToDelete = stockSymbol;
+  }
 }
 
-function checkStockAlreadyExist(stock) {
-  let watchlist = [];
-  return new Promise(resolve => {
-    chrome.storage.local.get(['stocks'], function (result) {
-      if (result.stocks != null) {
-        watchlist = result.stocks;
-        const found = watchlist.find(({ symbol }) => symbol === stock.symbol)
-        if (found !== undefined) {
-          resolve(true)
-        }
-        else {
-          resolve(false);
-        }
-      }
-    });
-  })
+function handleDelete(e) {
+  const stockSymbol = state.selectedStockToDelete;
+  console.log(stockSymbol);
+  if (stockSymbol !== "") {
+    // delete from localstorage
+    let stocks = [];
+    stocks = JSON.parse(localStorage.getItem("stocks"));
+    if (stocks == undefined) {
+      stocks = [];
+    }
+    stocks = stocks.filter((stock) => stock !== stockSymbol);
+    localStorage.setItem("stocks", JSON.stringify(stocks));
+    state.selectedStockToDelete = "";
+    ui.displayToastMessage(
+      `${stockSymbol} have been successfully deleted from the watchlist`,
+      "success"
+    );
+  } else {
+    console.log("Error selectedStockToDelete is empty");
+  }
+  getDataAndDisplay();
 }
